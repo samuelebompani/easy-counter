475df48cbce60fd927ef7176f39f59427edaa34d
samuele <samuelebompani@gmail.com>
2021-01-15 23:49:33 +0100

    App is working, few problems dark mode

--- app/src/main/java/com/example/easycounter/Counter.kt
+++ app/src/main/java/com/example/easycounter/Counter.kt
@@ -1,2 +1,20 @@
 package com.example.easycounter
 
+class Counter (var n : Int){
+
+    fun add() {
+        this.n++
+    }
+
+    fun sub() {
+        this.n--
+    }
+
+    fun reset() {
+        this.n=0
+    }
+
+    fun print() : String {
+        return this.n.toString()
+    }
+}
--- app/src/main/java/com/example/easycounter/MainActivity.kt
+++ app/src/main/java/com/example/easycounter/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.easycounter
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +10,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val shared = getSharedPreferences("user_data", MODE_PRIVATE)
+
+        //Get the value stored in memory
+        val oldCounter = shared.getInt("counter", 0)
+        val counter = Counter(oldCounter)
+        val display : TextView = findViewById(R.id.display)
+        val plusButton : Button = findViewById(R.id.button_plus)
+        val minusButton : Button = findViewById(R.id.button_minus)
+
+        //Initialize the display with the value memorized or with the default value 0
+        display.text = counter.print()
+
+        //Button plus increases the value by one, button minus subtracts one from the value
+        plusButton.setOnClickListener {
+            counter.add()
+            shared.edit().putInt("counter", counter.n).apply()
+            display.text = counter.print()
+        }
+
+        minusButton.setOnClickListener {
+            counter.sub()
+            shared.edit().putInt("counter", counter.n).apply()
+            display.text = counter.print()
+        }
     }
 }
