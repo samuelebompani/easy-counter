e5955001a085190eed05b7172d9efca3c974607a
samuele <samuelebompani@gmail.com>
2021-01-26 23:12:08 +0100

    Add button, limit to the number, exceptions handled

--- app/src/main/java/com/example/easycounter/Counter.kt
+++ app/src/main/java/com/example/easycounter/Counter.kt
@@ -1,19 +1,19 @@
 package com.example.easycounter
 
+import java.lang.IllegalArgumentException
+
 class Counter (var n : Int){
 
     fun set(m : Int) {
-        if (m<0)
-            this.n = 0
-        else
-            this.n = m
+        if (m < 0 || m >= 10000000) //i don't want big numbers in the
+            throw IllegalArgumentException()
+        this.n = m
     }
 
     fun add(m : Int) {
-        if (n+m < 0)
-            this.n = 0
-        else
-            this.n = n+m
+        if (n+m < 0 || n+m >= 10000000)
+            throw IllegalArgumentException()
+        this.n = n+m
     }
 
     fun print() : String {
