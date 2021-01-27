1bb94113e6a4f79e1f3e11dfa7f2e0bf54a22f04
samuele <samuelebompani@gmail.com>
2021-01-27 01:10:12 +0100

    all files added

---
+++ app/src/main/java/com/easy/counter/Counter.kt
@@ -0,0 +1,22 @@
+package com.easy.counter
+
+import java.lang.IllegalArgumentException
+
+class Counter (var n : Int){
+
+    fun set(m : Int) {
+        if (m < 0 || m >= 10000000) //i don't want big numbers in the
+            throw IllegalArgumentException()
+        this.n = m
+    }
+
+    fun add(m : Int) {
+        if (n+m < 0 || n+m >= 10000000)
+            throw IllegalArgumentException()
+        this.n = n+m
+    }
+
+    fun print() : String {
+        return this.n.toString()
+    }
+}
